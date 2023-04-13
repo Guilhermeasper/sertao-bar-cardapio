4f1b0c725ddc9080bb611de210ffbad2550e3e58
Guilherme Afonso <gasp@cin.ufpe.br>
2023-04-12 23:41:31 -0300

    feat: add api url and key to google spreasheet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url:
+    'https://sheets.googleapis.com/v4/spreadsheets/1PZrpD4N5ELucLNpw9AMl0-GOlhd6WejX5GyZROCA0wo/values/',
+  api_key: 'AIzaSyA5pZowMU-sgg0p1isdOlHrXwrfm7FDgo8',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  api_url:
+    'https://sheets.googleapis.com/v4/spreadsheets/1PZrpD4N5ELucLNpw9AMl0-GOlhd6WejX5GyZROCA0wo/values/',
+  api_key: 'AIzaSyA5pZowMU-sgg0p1isdOlHrXwrfm7FDgo8',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
