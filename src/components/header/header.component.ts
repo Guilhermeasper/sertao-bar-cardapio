b38800b5d9ecbd3b3438ffa00d8b31d91f8a8677
Guilherme Afonso <guilhermeafonso208@gmail.com>
2023-08-05 03:37:10 -0300

    feat: add loading animation on start

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -11,23 +11,22 @@ import { CommonModule } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule],
   standalone: true,
-  template: `
-    <div class="name">
-      <img src="../../assets/horizontal-light.svg" />
-    </div>
-  `,
+  template: ` <img src="../../assets/horizontal-light.svg" /> `,
   styles: [
     `
       :host {
         display: flex;
-        flex-direction: column;
-        flex: 1;
         width: 100%;
         z-index: 10;
         position: relative;
         position: fixed;
         height: 8vh;
         background-color: var(--sertao-light);
+        transition-property: height, background-color;
+        transition-duration: 0.75s;
+        transition-timing-function: ease-out;
+        justify-content: center;
+        align-items: center;
 
         &.scroll-shadow {
           --shadow-color: #65250b0b;
@@ -35,17 +34,53 @@ import { CommonModule } from '@angular/common';
           box-shadow: 1px 2px 2px var(--shadow-color),
             2px 4px 4px var(--shadow-color), 3px 6px 6px var(--shadow-color);
         }
-      }
 
-      .name {
-        height: 4rem;
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+        &.loading {
+          background-color: transparent;
+          -webkit-animation: breathing 3s ease infinite;
+          animation: breathing 3s ease infinite;
+          height: 100%;
+        }
 
         img {
           height: 2rem;
+        }
+      }
+
+      @-webkit-keyframes breathing {
+        0% {
+          -webkit-transform: scale(0.9);
+          transform: scale(0.9);
+        }
+
+        60% {
+          -webkit-transform: scale(1);
+          transform: scale(1);
+        }
+
+        100% {
+          -webkit-transform: scale(0.9);
+          transform: scale(0.9);
+        }
+      }
+
+      @keyframes breathing {
+        0% {
+          -webkit-transform: scale(0.9);
+          -ms-transform: scale(0.9);
+          transform: scale(0.9);
+        }
+
+        60% {
+          -webkit-transform: scale(1);
+          -ms-transform: scale(1);
+          transform: scale(1);
+        }
+
+        100% {
+          -webkit-transform: scale(0.9);
+          -ms-transform: scale(0.9);
+          transform: scale(0.9);
         }
       }
     `,
@@ -53,4 +88,5 @@ import { CommonModule } from '@angular/common';
 })
 export class SBHeaderComponent {
   @Input() @HostBinding('class.scroll-shadow') scrollShadow = false;
+  @Input() @HostBinding('class.loading') loading = true;
 }
