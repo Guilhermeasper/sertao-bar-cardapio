d5ed3c6542d5e4039beec0e2b09968b9315a74fd
Guilherme Afonso <gasp@cin.ufpe.br>
2023-08-05 02:31:20 -0300

    fix: remove category chip list from header

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,39 +1,23 @@
 import {
   Component,
-  OnInit,
   ChangeDetectionStrategy,
   Input,
-  Output,
-  EventEmitter,
-  ViewChild,
-  ElementRef,
+  HostBinding,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SBChipComponent } from '@sertao-bar/components/chip/chip.component';
 
 @Component({
   selector: 'sb-header',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, SBChipComponent],
+  imports: [CommonModule],
   standalone: true,
   template: `
     <div class="name">
       <img src="../../assets/horizontal-light.svg" />
     </div>
-    <div *ngIf="menuLoaded" #menu class="menu-select">
-      <sb-chip
-        *ngFor="let item of sections"
-        [name]="item"
-        [active]="item === selectedSection"
-        (stateChange)="changeSection($event)"
-        >{{ item }}</sb-chip
-      >
-    </div>
   `,
   styles: [
     `
-      @import url('https://fonts.googleapis.com/css2?family=Titan+One&display=swap');
-
       :host {
         display: flex;
         flex-direction: column;
@@ -42,9 +26,15 @@ import { SBChipComponent } from '@sertao-bar/components/chip/chip.component';
         z-index: 10;
         position: relative;
         position: fixed;
+        height: 8vh;
         background-color: var(--sertao-light);
 
-        box-shadow: 0 4px 12px 0 #0000001a;
+        &.scroll-shadow {
+          --shadow-color: #65250b0b;
+
+          box-shadow: 1px 2px 2px var(--shadow-color),
+            2px 4px 4px var(--shadow-color), 3px 6px 6px var(--shadow-color);
+        }
       }
 
       .name {
@@ -58,52 +48,9 @@ import { SBChipComponent } from '@sertao-bar/components/chip/chip.component';
           height: 2rem;
         }
       }
-
-      .menu-select {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        gap: 0.5rem;
-        height: 3.5rem;
-        width: 100%;
-        overflow-y: hidden;
-        overflow-x: scroll;
-        padding-bottom: 1rem;
-        z-index: 20;
-
-        :first-child {
-          margin-left: 2rem;
-        }
-
-        :last-child {
-          margin-right: 2rem;
-        }
-
-        &::-webkit-scrollbar,
-        &::-webkit-scrollbar-button,
-        &::-webkit-scrollbar-track-piece,
-        &::-webkit-scrollbar-track,
-        &::-webkit-scrollbar-thumb {
-          height: 0;
-        }
-      }
     `,
   ],
 })
-export class SBHeaderComponent implements OnInit {
-  @Output() sectionChanged: EventEmitter<string> = new EventEmitter();
-
-  @Input() menuLoaded = false;
-  @Input() selectedSection = '';
-  @Input() sections: string[] = [];
-
-  @ViewChild('menu') menu: ElementRef<HTMLElement> | undefined;
-
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  changeSection(event: string) {
-    this.sectionChanged.emit(event);
-  }
+export class SBHeaderComponent {
+  @Input() @HostBinding('class.scroll-shadow') scrollShadow = false;
 }
